dd7adfc68005045e0df7e51b57e57769c2b9b27d
Fahim1113 <79474484+Fahim1113@users.noreply.github.com>
2022-07-12 21:03:18 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+import dropbox
+
+class TransferData:
+    def __init__(self, access_token):
+        self.access_token = access_token
+
+    def upload_file(self, file_from, file_to):
+        dbx = dropbox.Dropbox(self.access_token)
+
+
+        f = open(file_from, 'rb')
+        dbx.files_upload(f.read(), file_to)
+
+def main():
+    access_token = 'sl.BLQX6akn2FVgVNBPNuPzVAHiyZ98nztArAObHwl_es3v06aAeui0swbS39IQ9jMTW8VkzAn3WhS2T7m_NRmm4EwaKmJvhcj8uWUZlScT8i41jwIQthd5lSL4nnSUZpVWKuTUV5w'
+    transferData = TransferData(access_token)
+
+    file_from = input("Enter the file path to transfer : -")
+    file_to = input("enter the full path to upload to dropbox:- ")  # This is the full path to upload the file to, including name that you wish the file to be called once uploaded.
+#C:/Users/fahim/Desktop/Byjus future school python/c101/a
+#/c101
+    # API v2
+    transferData.upload_file(file_from, file_to)
+    print("file has been moved !!!")
+
+
+main()
